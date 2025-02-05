76a35fbd7246993787bae9aa8f71b7045a267fd6
Sebastian Dehnbostel <sebastian91w@gmail.com>
2025-02-05 18:37:07 +0100

    set_block() in board

--- board/board.gd
+++ board/board.gd
@@ -1,12 +1,22 @@
 class_name Board
 extends TileMapLayer
 
-class ScoreField:
-	var blocked: int
-	var group: Array[ScoreField]
-	
-@onready var score_maps = get_children()
-var score_map: Dictionary = {}
+class BlockSensor:
+	var blocked: bool
+	var layers: Array[TileMapLayer]
+
+	func _init(blocked: bool, layers: Array[TileMapLayer]) -> void:
+		self.blocked = blocked
+		self.layers = layers
+
+	func _to_string() -> String:
+		return str(blocked, " ", str(layers))
+
+
+var block_sensor_from_cell: Dictionary = {}
+
+func _ready() -> void:
+	make_score_map()
 
 func drop_shape(shape: Shape):
 	var loc = to_local(shape.position)
@@ -15,8 +25,20 @@ func drop_shape(shape: Shape):
 		return
 	shape.position = map_to_local(cell) + position
 	shape.grabbable = false
+	for block in shape.blocks:
+		set_block(block)
 
-func make_scores():
-	for layer in score_maps:
-		var tile_map_layer: TileMapLayer = layer
-		var cells = tile_map_layer.get_used_cells()
+func make_score_map():
+	for cell in get_used_cells():
+		var layers: Array[TileMapLayer] = []
+		for layer: TileMapLayer in get_children():
+			if layer.get_cell_tile_data(cell) != null:
+				layers.append(layer)
+		var block_sensor: BlockSensor = BlockSensor.new(false, layers)
+		block_sensor_from_cell.get_or_add(cell, block_sensor)
+
+func set_block(block: Block):
+	var cell = local_to_map((block.position))
+	var block_sensor: BlockSensor = block_sensor_from_cell[cell]
+	block_sensor.blocked = true
+	print(block_sensor_from_cell[cell])
--- tests/test_board.gd
+++ tests/test_board.gd
@@ -1,9 +1,21 @@
 extends Node2D
 
+var frame: int = 0
 @onready var board: Board = $Board
+@onready var l_shape: Shape = $LShape
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	for node in board.score_maps:
-		var map: TileMapLayer = node
-		print(map.get_used_cells())
+	print(board.block_sensor_from_cell)
+	print()
+
+func _on_l_shape_released(shape: Shape) -> void:
+	board.drop_shape(shape)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed and event.button_index == MOUSE_BUTTON_RIGHT:
+			if frame == 8:
+				return
+			frame += 1
+			l_shape.set_frame(frame)
